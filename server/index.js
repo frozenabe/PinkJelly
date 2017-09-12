9bfb358c380f6227e41c594469601a965836cfa7
pre-12-HyungonKim <frozenabe86@gmail.com>
2017-09-12 11:43:43 +0900

    Pink Jelly Starter

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// const admin = require("firebase-admin");
+
+// const serviceAccount = require("gs://fir-demo-project.appspot.com/");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert({
+//     projectId: process.env.PROJECT_ID,
+//     clientEmail: process.env.SERVICE_ACCOUNT_ID,
+//     privateKey: process.env.PRIVATE_KEY,
+//   }),
+//   databaseURL: process.env.DATABASE_URL,
+// });
