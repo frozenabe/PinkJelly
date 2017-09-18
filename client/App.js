bb7a693e0f8c43ac47c4bbf06da74c04ccda9522
김요한 <yodoree@gim-yohan-ui-MacBook-Pro.local>
2017-09-18 17:02:22 +0900

    login complete

--- client/App.js
+++ client/App.js
@@ -1,17 +1,68 @@
 import React, { Component } from 'react';
 import { StyleSheet, View} from 'react-native';
 import { RootNavigation } from './navigation/RootNavigation';
+import * as firebase from 'firebase';
+import Login from './component/Login.js';
+import EmailVerify from './component/EmailVerify'
 
 export default class App extends Component {
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <RootNavigation />
-      </View>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false
+    };
   }
 
+  componentWillMount () {
+    const config = {
+    apiKey: "AIzaSyBL4NufHmm1JthU7Xy0Mrw0oWakEDowLtU",
+    authDomain: "lateral-spirit-179615.firebaseapp.com",
+    databaseURL: "https://lateral-spirit-179615.firebaseio.com",
+    projectId: "lateral-spirit-179615",
+    storageBucket: "lateral-spirit-179615.appspot.com",
+    messagingSenderId: "999843785302"
+  };
+  firebase.initializeApp(config);
+  // firebase.auth().createUserWithEmailAndPassword('ddd@naver.com', '1111111');
+  }
+
+  componentDidMount() {
+    firebase.auth().signOut().then(() => {
+    })
+      .catch((err) => {
+        console.log(err);
+      });
+    const _this = this;
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        _this.setState({ isLoading: true });
+      } else {
+        _this.setState({ isLoading: false });
+      }
+    });
+  }
+
+  render() {
+    if (!this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <Login />
+        </View>
+      );
+    } else if (this.state.isLoading && !firebase.auth().currentUser.emailVerified) {
+      return (
+        <View style={styles.container}>
+          <EmailVerify />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <RootNavigation />
+        </View>
+      )
+    }
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ client/component/Login.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+} from 'react-native';
+import * as firebase from 'firebase';
+import SignUp from './SignUp'
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      signUp: false,
+    };
+  }
+
+  loginClick() {
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log(user.emailVerified);
+        alert('로그인 되셧습니다');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
+  signUpClick() {
+    this.setState({ signUp: true });
+  }
+
+  render() {
+    if (!this.state.signUp) {
+    return (
+      <View style={styles.container}>
+        <TextInput
+         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+         placeholder="email"
+         onChangeText={text => this.setState({email: text})}
+        />
+        <TextInput
+         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+         placeholder="password"
+         onChangeText={(text) => this.setState({password: text})}
+        />
+        <Button
+          onPress = {this.loginClick.bind(this)}
+          title="login"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress = {this.signUpClick.bind(this)}
+          title="signup"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    )
+  }
+    return (
+      <View style={styles.container}>
+       <SignUp />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
