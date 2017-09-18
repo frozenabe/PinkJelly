e98213fcfa7d743f1f96932b85b0d99ab0c69a35
zao1214 <hexa1214@gmail.com>
2017-09-18 16:56:55 +0900

    client recieve Object Data

--- client/screens/CameraScreen.js
+++ client/screens/CameraScreen.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { Camera, Permissions } from 'expo';
 import axios from 'axios';
+import { RNS3 } from 'react-native-aws3';
 
 import Shutter from '../components/Shutter';
 
 export default class CameraScreen extends Component {
   state = {
     hasCameraPermission: null,
-    photoID: 0,
   };
 
   async componentWillMount() {
@@ -19,18 +19,46 @@ export default class CameraScreen extends Component {
   snapshot() {
     this.camera.takePictureAsync()
       .then((data) => {
-        axios.post('/', {
-          imagePath: data,
-        })
-        .then(res => console.log(res))
+        const file = {
+          uri: data,
+          name: "image.jpg",
+          type: "image/jpg"
+        }
+
+
+        const options = {
+          keyPrefix: "images/",
+          bucket: "foxtailbucket",
+          region: "ap-northeast-2",
+          accessKey: "AKIAIIUSH444NSDBVBNA",
+          secretKey: "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",
+          successActionStatus: 201
+        }
+
+        RNS3.put(file, options).then(response => {
+          if (response.status !== 201) {
+            throw new Error("Failed to upload image to S3");
+          }
+          console.log(response.body);
+        });
+
+        this.props.setImagePath(data);
       })
+      .then(() => {
+        axios.get('/')
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => console.log(err));
+      })
+      .then(() => this.props.slideTrigger())
       .catch(err => console.log(err));
   }
 
   render() {
     const { hasCameraPermission } = this.state;
     if (hasCameraPermission === null) {
-      return <View />;
+      return <Text>ssibal</Text>;
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
     } else {
